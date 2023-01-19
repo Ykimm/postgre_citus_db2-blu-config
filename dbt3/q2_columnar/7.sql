c8e05f139454f62b9a2f894aea8e44653e1576e0
Ykimm <59131667+Ykimm@users.noreply.github.com>
2023-01-19 22:46:12 +0900

    Update 7.sql

--- dbt3/q2_columnar/7.sql
+++ dbt3/q2_columnar/7.sql
@@ -13,15 +13,15 @@ from
 		select
 			n1.n_name as supp_nation,
 			n2.n_name as cust_nation,
-			extract(year from l_shipdate) as l_year,
+			extract(year from l_shipdate_columnar) as l_year,
 			l_extendedprice * (1 - l_discount) as volume
 		from
-			supplier,
-			lineitem,
-			orders,
-			customer,
-			nation n1,
-			nation n2
+			supplier_columnar,
+			lineitem_columnar,
+			orders_columnar,
+			customer_columnar,
+			nation_columnar n1,
+			nation_columnar n2
 		where
 			s_suppkey = l_suppkey
 			and o_orderkey = l_orderkey
